566a2680c00a9de3ff9ca27ae0966deab7a79b32
Ethan Rowan <erowan@uoguelph.ca>
2022-09-14 19:41:40 -0400

    Improve project structure

---
+++ tests/test_courseparser.py
@@ -0,0 +1,11 @@
+import unittest
+import src.parser
+
+class testtest(unittest.TestCase):
+    def test(self):
+        # self.assertTrue(1==1, "good")
+        # testfunc()
+        None
+
+if __name__ == "__main__":
+    unittest.main()
