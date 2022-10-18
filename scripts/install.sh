74f8b38ca5a8f2fc86806293d27600851d1bbab6
Nash Rudiak <nrudiak@uoguelph.ca>
2022-10-17 21:28:57 -0400

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ sudo apt install nginx
 # Set up Flask
 sudo apt install python3
 pip3 install virtualenv
-python3 -m vnenv venv
+python3 -m venv venv
 . venv/bin/activate
 pip install Flask
 deactivate
