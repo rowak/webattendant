a6392d44147fbe513cf5e20057c8aaebd49fb88b
rowak <rowak12@gmail.com>
2022-10-17 21:40:40 -0400

    Fix flask install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,8 @@ sudo apt install nginx
 
 # Set up Flask
 sudo apt install python3
-pip3 install virtualenv
+sudo apt install python3-pip
+pip install virtualenv
 python3 -m venv venv
 . venv/bin/activate
 pip install Flask
