19c7a9d4dde6f2f3a1f8d2a01263be0a0a5fa0cb
rowak <rowak12@gmail.com>
2022-10-18 00:04:39 -0400

    Update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,27 @@
+APPDIR = /app
+
+sudo apt update
+
 # Set up nginx
-sudo apt install nginx
+sudo apt install nginx -y
+sudo cp config/nginx/sites-available/cis3760.conf /etc/nginx/sites-available
+sudo ln -s /etc/nginx/sites-available/cis3760.conf /etc/nginx/sites-enabled
+sudo systemctl restart nginx
 
 # Set up Flask
-sudo apt install python3
-sudo apt install python3-pip
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo apt install python3-venv -y
 pip install virtualenv
 python3 -m venv venv
 . venv/bin/activate
 pip install Flask
+
+# Set up Gunicorn
+sudo apt install gunicorn -y
+pip install gunicorn
+sudo cp config/gunicorn/cis3760.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl restart cis3760.service
+
 deactivate
