73c5f5b96156333fcdbc04b634ded7b3a1715553
rowak <rowak12@gmail.com>
2022-10-27 20:56:51 -0400

    Add basic searching

--- backend/flaskServer.py
+++ backend/flaskServer.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, Response
 import json
 import random
 
@@ -15,11 +15,52 @@ def homePage():
 def allCourses():
     return courseList
 
-@app.route('/getCourse', methods=['POST'])
-def getCourse(i):
-    return courseList[i]
+@app.route('/getCourse', methods=['GET'])
+def getCourse():
+    if "code" not in request.args:
+        return Response(json.dumps({"error": "No course specified."}), status=400)
+    
+    code = request.args["code"]
+    if "sectionCode" in request.args:
+        section = request.args["sectionCode"]
+    else:
+        section = None
+    course = findCourse(code, section)
+
+    if course == None:
+        return Response(json.dumps({"error": "Course not found."}), status=404)
+    else:
+        return course
 
 @app.route('/randomCourse', methods=['GET'])
 def randomCourse():
     i = random.randint(0, len(courseList) - 1)
     return courseList[i]
+
+# Finds a course in the course list based on a specific
+# course code and (optionally) a section code.
+def findCourse(code, sectionCode):
+    code = code.upper().replace("*", "")
+    if sectionCode != None:
+        sectionCode = sectionCode.lstrip("0")
+    for course in courseList:
+        if course["code"].replace("*", "") == code:
+            if sectionCode != None:
+                for section in course["sections"]:
+                    if section["code"].lstrip("0") == sectionCode:
+                        return getCourseWithSection(course, sectionCode)
+                return None
+            else:
+                return course
+    return None
+
+# Returns a copy of a course object with only a specific section
+# (all others removed).
+def getCourseWithSection(course, sectionCode):
+    courseCopy = course.copy()
+    sections = []
+    for section in course["sections"]:
+        if section["code"].lstrip("0") == sectionCode:
+            sections.append(section)
+    courseCopy["sections"] = sections
+    return courseCopy
