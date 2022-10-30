291e0b0c385224c1fac613e12056c35b55ff54c0
rowak <rowak12@gmail.com>
2022-10-29 22:22:43 -0400

    Add environment-based API URL in react

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,9 @@
 from flaskServer import app
+import os
 
 if __name__ == "__main__":
-    app.run()
+    if os.environ.get("FLASK_ENVIRONMENT", "") == "development":
+        app.run()
+    else:
+        context = ("/etc/ssl/certs/ssl_api_cert.crt", "/etc/ssl/private/ssl_api_cert.key")
+        app.run(ssl_context=context)
