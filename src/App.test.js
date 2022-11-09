e5a7447c244b74f16ddd9719d5734b4fd50af144
rowak <rowak12@gmail.com>
2022-11-09 11:40:26 -0500

    Remove comments

--- src/App.test.js
+++ src/App.test.js
@@ -2,12 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test('renders header text', () => {
   render(<App />);
   expect(screen.getByRole("heading", {level: 1})).toHaveTextContent("WebAttendant");
