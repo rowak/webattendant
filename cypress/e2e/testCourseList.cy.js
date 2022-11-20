75c809c37bdd2a0b58af54966f4fc33fc8347cb0
Ameer Mushani <amushani@uoguelph.ca>
2022-11-20 17:28:37 -0500

    Fix course list tests

--- cypress/e2e/testCourseList.cy.js
+++ cypress/e2e/testCourseList.cy.js
@@ -2,11 +2,11 @@
 describe('testSearch', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
+        cy.get('.courseInput').type('CIS*3750')
+        cy.get('.courseSearchContent > .mb-3 > .btn').click()
       })
     
     it('course add to list', () => {
-        cy.get('.courseInput').type('CIS*3750')
-        cy.get('.courseSearchContent > .mb-3 > .btn').click()
 
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > .btn.btn-primary').first().click()
         cy.get('.scheduledCoursesList >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
@@ -15,9 +15,7 @@ describe('testSearch', () => {
     })
 
         
-    it('course add to list', () => {
-        cy.get('.courseInput').type('CIS*3750')
-        cy.get('.courseSearchContent > .mb-3 > .btn').click()
+    it('course add to list then remove', () => {
 
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > .btn.btn-primary').first().click()
         cy.get('.scheduledCoursesList >  .courseList > .courseListGroup > .courseListItem > .btn').first().click()
