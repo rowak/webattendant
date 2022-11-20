e9d11ce2fca4d60b80b5146f18a3a135be263d31
Ameer Mushani <amushani@uoguelph.ca>
2022-11-20 17:05:24 -0500

    Updated test names and gitignore

--- cypress/e2e/testSearch.cy.js
+++ cypress/e2e/testSearch.cy.js
@@ -4,37 +4,37 @@ describe('testSearch', () => {
         cy.visit('http://localhost:3000/')
       })
     
-    it('passes', () => {
+    it('regular search', () => {
         cy.get('.courseInput').type('CIS*3750')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
     })
     
-    it('passes', () => {
+    it('lowercase search', () => {
         cy.get('.courseInput').type('cis*3750')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
     })
     
-    it('passes', () => {
+    it('irregular capitalization', () => {
         cy.get('.courseInput').type('cIs*3750')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
     })
     
-    it('passes', () => {
+    it('no asterisk', () => {
         cy.get('.courseInput').type('CIS3750')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
     })
 
-    it('passes', () => {
+    it('lowercase no asterisk', () => {
         cy.get('.courseInput').type('cis3750')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > h5').should('contain', 'CIS*3750')
     })
 
-    it('passes', () => {
+    it('introduction', () => {
         cy.get('.courseInput').type('Introduction')
         cy.get('.courseSearchContent > .mb-3 > .btn').click()
         cy.get('.courseSearchContent >  .courseList > .courseListGroup > .courseListItem > div > p').should('contain', 'Introduction')
