849ac8311b200666cb7079c869388504e89d88e0
Dogu Gerger <dgerger@uoguelph.ca>
2022-11-20 21:57:06 -0500

    linting changes

--- src/Components/MeetingInfoList.test.js
+++ src/Components/MeetingInfoList.test.js
@@ -6,9 +6,9 @@ import React from 'react'; // Required to interact with the React DOM
 test('MeetingInfoList rendered', () => {
     let meetings = [];
 
-    const { container } = render(<MeetingInfoList meetings={meetings} />);
+    render(<MeetingInfoList meetings={meetings} />);
 
-    expect(container.firstChild.classList.contains('meeting-grid')).toBe(true)
+    expect(screen.getByRole("table")).toHaveClass("meeting-grid");
 
 });
   
