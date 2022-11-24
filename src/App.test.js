b3406892c04f45a78e464ba88213ad184bc3f736
rowak <rowak12@gmail.com>
2022-11-24 12:42:31 -0500

    Fix linting warnings

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+// import React from 'react';
 
 test('renders header text', () => {
   // Testing really does not like the router library, so I have commented it
